2eade70fbc0ef366f9124055a8426cd2f6b862a9
Selt0 <chewedgum12@gmail.com>
2020-05-14 12:23:18 -0500

    Add tests for card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,11 +3,23 @@ require 'card'
 
 describe Card do 
   describe '#initialize' do
-    subject(:card) { Card.new(10, '♥') }
+    subject(:card) { Card.new(10, :hearts) }
 
     it 'sets up a card correctly' do
-      expect(card.suit).to eq('♥')
+      expect(card.suit).to eq(:hearts)
       expect(card.value).to eq(10)
+    end
+
+    context 'invalid suit' do
+      it 'raises an error' do
+        expect { Card.new(10, :test) }.to raise_error(RuntimeError)
+      end
+    end
+
+    context 'invalid value' do
+      it 'raises an error' do
+        expect { Card.new(20, :spades) }.to raise_error(RuntimeError)
+      end
     end
   end
 end
