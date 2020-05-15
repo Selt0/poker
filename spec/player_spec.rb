c0de59a690ac91b8d1d5bc43fdbd6e228b9737d8
Selt0 <chewedgum12@gmail.com>
2020-05-15 12:35:52 -0500

    add more tests to player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,7 +15,7 @@ describe Player do
   end
 
   describe '#deal_in' do
-    let(:hand) { double ('hand') }
+    let(:hand) { double('hand') }
 
     it "should set the players hand" do
       player.deal_in(hand)
@@ -45,6 +45,58 @@ describe Player do
 
     it "should return the amount deducted" do
       expect(player.take_bet(10)).to eq(10)
+    end
+  end
+
+  describe "#receive_winnings" do 
+    it "should increment the players balance by the amount won" do
+      expect { player.receive_winnings(10) }.to change { player.balance }.by(10)
+    end
+  end
+
+  describe "#return_cards" do
+    let(:hand) { double('hand') }
+    let(:cards) { double('cards') }
+
+    before(:each) do
+      player.deal_in(hand)
+      allow(hand).to receive(:cards).and_return(cards)
+    end
+
+    it "should return the players cards" do
+      expect(player.return_cards).to eq(cards)
+    end
+
+    it "should set the players hand to nill" do
+      player.return_cards
+      expect(player.hand).to be(nil)
+    end
+  end
+
+  describe "#fold" do
+    it "should set #folded? to true" do
+      player.fold
+      expect(player).to be_folded
+    end
+  end
+
+  describe "#unfold" do
+    it "should set #folded? to false" do
+      player.unfold
+      expect(player).to_not be_folded
+    end
+  end
+
+  describe '#folded?' do
+    let(:player) { Player.new(1000) }
+
+    it "should return true if player is folded" do
+      player.fold
+      expect(player).to be_folded
+    end
+
+    it "should return false otherwise" do
+      expect(player).to_not be_folded
     end
   end
 end
