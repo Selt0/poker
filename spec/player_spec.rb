4a818f0b95e10e53f99de76b3db9aeea792f8cb7
Selt0 <chewedgum12@gmail.com>
2020-05-15 12:18:06 -0500

    deal in and take_bet tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,4 +13,38 @@ describe Player do
       expect(Player.buy_in(100).balance).to eq(100)
     end
   end
+
+  describe '#deal_in' do
+    let(:hand) { double ('hand') }
+
+    it "should set the players hand" do
+      player.deal_in(hand)
+      expect(player.hand).to eq(hand)
+    end
+  end
+
+  describe "#take_bet" do
+    context "initial bet" do
+      it "should decrement the players balance" do
+        expect { player.take_bet(10) }.to change { player.balance }.by(-10)
+      end 
+    end
+
+    context "player raises bet" do
+      it "should decrement the players balance" do
+        player.take_bet(10)
+        expect{ player.take_bet(20) }.to change { player.balance }.by(-10)
+      end
+    end
+
+    context "if the bet is more than their balance" do
+      it "raises an error" do
+        expect { player.take_bet(1000) }.to raise_error "not enough money"
+      end
+    end
+
+    it "should return the amount deducted" do
+      expect(player.take_bet(10)).to eq(10)
+    end
+  end
 end
